c310fae18ea696b1d53349e0095abda856aa288a
alekskorolev3 <alekskorolev3@gmail.com>
2021-03-13 18:16:05 +0300

    Initial commit

--- deque_1/deque_1.cpp
+++ deque_1/deque_1.cpp
@@ -6,11 +6,9 @@ template <class T>
 class Block
 {
 public:
-    T* chunk;
+    T chunk[4];
     Block()
     {
-        chunk = new T[4];
-
     }
     T* GetChunk()
     {
@@ -38,20 +36,42 @@ public:
 
 
     }
-    void IsOneChunk(bool direction)
+    bool IsOneChunk(bool direction)
     {
-        if (start / 4 == end / 4)
+        if (direction)
         {
-            if (!direction)
+            int start_ch_index = start / 4;
+            int start_index = start % 4;
+            int end_ch_index = end  / 4;
+            int end_index = end % 4;
+            if (start_ch_index == end_ch_index && end_index < start_index)
             {
-                resize(false);
+                return true;
             }
-            else { resize(true); }
+            return false;
         }
+        else
+        {
+            int start_ch_index = start / 4;
+            int start_index = start % 4;
+            int end_ch_index = end  / 4;
+            int end_index = end % 4;
+            if (start_ch_index == end_ch_index && end_index < start_index)
+            {
+                return true;
+            }
+            return false;
+        }
+        
     }
     void push_back(T data)
     {
         bool direction = true;
+        
+        if (IsOneChunk(direction))
+        {
+            resize(direction);
+        }
         int buff = 0;
         buff = end / 4;
         int i = 0;
@@ -60,51 +80,72 @@ public:
         temp->chunk[i] = data;
         end += 1;
         end %= adress.size() * 4;
-
+        size++;
 
     }
     void push_front(T data)
     {
         bool direction = false;
+        
         start += adress.size() * 4 - 1;
         start %= adress.size() * 4;
+        if (IsOneChunk(direction))
+        {
+            resize(direction);
+        }
         int chunk_index = start / 4;
         int i = start % 4;
-        IsOneChunk(direction);
         Block<T>* temp = adress[chunk_index];
         temp->chunk[i] = data;
+        size++;
     }
     void resize(bool flag)
     {
         if (flag)
         {
             int chunk_index = end / 4;
-            auto it = adress.begin();
+            
             for (int i = 0; i < capacity; i++)
             {
-                adress.insert(it + i + chunk_index, new Block<T>());
+                auto it = adress.begin();
+                adress.insert(it + chunk_index, new Block<T>());
             }
+            
+            end += capacity * 4;
             capacity *= 2;
+            
         }
         else
         {
-            int chunk_index = start / 4;
-            auto it = adress.begin();
+            int chunk_index = (start + 1) / 4;
+            
             for (int i = 0; i < capacity; i++)
             {
-                adress.insert(it + i + chunk_index, new Block<T>());
+                auto it = adress.begin();
+                adress.insert(it + chunk_index, new Block<T>());
             }
+            start += capacity * 4;
             capacity *= 2;
+            
         }
         
     }
     void PrintElement(int index)
     {
-        int chunk_index = (start + index) % 12;
+        int chunk_index = (start + index) % capacity*4;
         int buff = chunk_index / 4;
         int i = chunk_index % 4;
         Block<T>* temp = adress[buff];
         cout << temp->chunk[i]<< endl;
+        /*for (int i = 0; i < adress.size(); i++)
+        {
+            Block<T>* temp = adress[i];
+            for (int j = 0; j < 4; j++)
+            {
+                cout << temp->chunk[j] << " ";
+            }
+            cout << endl;
+        }*/
         
     }
 private:
@@ -118,19 +159,21 @@ int main()
     deque <int> d;
     
     d.push_front(100);
+    d.push_back(10);
     d.push_front(200);
+    d.push_back(20);
     d.push_front(300);
     d.push_front(400);
     d.push_front(500);
-
-    d.push_back(10);
-    d.push_back(20);
+    d.push_front(600);
+    d.push_front(700);
     d.push_back(30);
     d.push_back(40);
     d.push_back(50);
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < d.size; i++)
     {
         d.PrintElement(i);
     }
+    
 }
 
